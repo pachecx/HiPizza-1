a4b7cb167235872eecaee2b1aa77a6952aa6b50b
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-12-26 10:15:20 -0300

    feat: add entidade estabelecimento

---
+++ demo/src/main/java/com/hipizza/demo/domain/Estabelecimento.java
@@ -0,0 +1,33 @@
+package com.hipizza.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Estabelecimento")
+@Data
+public class Estabelecimento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nome")
+    private String nome;
+    @Column(name = "cpf")
+    private String cpf;
+    @Column(name = "email")
+    private String email; // Provisório até fazer a parte de autenticação
+    @Column(name = "telefone")
+    private String telefone;
+    @Column(name = "endereco")
+    private String endereco;
+    @Column(name = "cnpj")
+    private String cnpj;
+    @Column(name = "razao_social")
+    private String razao_social;
+    @Column(name = "agencia")
+    private String agencia;
+    @Column(name = "conta_corrente")
+    private String conta_corrente;
+    @Column(name = "senha")
+    private String senha; // Provisório até fazer a parte de autenticação
+}
