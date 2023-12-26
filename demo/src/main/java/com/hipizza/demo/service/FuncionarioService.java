477e9b47333075110a8b65769b94e45fcffa56b9
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2023-12-26 12:00:30 -0300

    feat: add service e repository de funcionario

---
+++ demo/src/main/java/com/hipizza/demo/service/FuncionarioService.java
@@ -0,0 +1,33 @@
+package com.hipizza.demo.service;
+
+import com.hipizza.demo.domain.Funcionario;
+import com.hipizza.demo.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public void cadastratFuncionario(Funcionario funcionario) {
+        funcionarioRepository.save(funcionario);
+    }
+
+
+    public void excluirFuncionarioPorId(Long id) {
+        funcionarioRepository.deleteById(id);
+    }
+
+    public List<Funcionario> listarFuncionarios() {
+        return funcionarioRepository.findAll();
+    }
+
+    public Funcionario buscarFuncionarioPorId(Long id) {
+        return funcionarioRepository.findById(id).orElse(null);
+    }
+}
+}
