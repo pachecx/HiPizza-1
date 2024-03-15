e1c331e32ebf4424344ed04f5c5a25736deb37ee
liscandeia <liscandeia523@gmail.com>
2024-03-15 10:19:31 -0300

    Subindo config swaggger + Spring Security

--- demo/src/main/java/com/hipizza/demo/controller/FuncionarioController.java
+++ demo/src/main/java/com/hipizza/demo/controller/FuncionarioController.java
@@ -5,6 +5,8 @@ import com.hipizza.demo.domain.Estabelecimento;
 import com.hipizza.demo.domain.Funcionario;
 import com.hipizza.demo.service.EstabelecimentoService;
 import com.hipizza.demo.service.FuncionarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -14,34 +16,36 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/funcionario")
+@Tag(name = "Controle de funcionários")
 public class FuncionarioController {
 
     @Autowired
     private FuncionarioService funcionarioService;
 
+    @Operation(summary = "Cadastrar um funcionário")
     @PostMapping(value = "/cadastrar")
     public ResponseEntity<String> incluir(@Validated @RequestBody Funcionario funcionario) {
         funcionarioService.cadastrarFuncionario(funcionario);
         return ResponseEntity.ok("Funcionário cadastrado com sucesso!");
     }
-
+    @Operation(summary = "Listar todos os funcionários")
     @GetMapping(value = "/listar")
     public ResponseEntity<Object> telaLista() {
         return ResponseEntity.ok(funcionarioService.listarFuncionarios());
     }
-
+    @Operation(summary = "Listar funcionários por estabelecimento")
     @GetMapping("/listar/{idEstabelecimento}")
     public ResponseEntity<List<Funcionario>> getFuncionariosPorEstabelecimento(@PathVariable Long idEstabelecimento) {
         List<Funcionario> funcionarios = funcionarioService.getFuncionariosPorEstabelecimento(idEstabelecimento);
         return ResponseEntity.ok(funcionarios);
     }
-
+    @Operation(summary = "Excluir um funcionário por ID")
     @DeleteMapping(value = "/{id}/excluir")
     public ResponseEntity<String> excluir(@PathVariable Long id) {
         funcionarioService.excluirFuncionarioPorId(id);
         return ResponseEntity.ok("Funcionário excluído com sucesso!");
     }
-
+    @Operation(summary = "Atualizar um funcionário por ID")
     @PutMapping(value = "/{id}/alterar")
     public ResponseEntity<String> alterar(@PathVariable Long id, @Validated @RequestBody Funcionario funcionario) {
         funcionario = funcionarioService.atualizarFuncionario(id, funcionario);
--- demo/src/main/java/com/hipizza/demo/controller/PedidoController.java
+++ demo/src/main/java/com/hipizza/demo/controller/PedidoController.java
@@ -2,6 +2,8 @@ package com.hipizza.demo.controller;
 
 import com.hipizza.demo.domain.Pedido;
 import com.hipizza.demo.service.PedidoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,10 +15,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pedido")
+@Tag(name = "Controle de pedidos")
 public class PedidoController {
     @Autowired
     private PedidoService pedidoService;
 
+    @Operation(summary = "Fazer um pedido")
     @PostMapping("/fazer")
     public ResponseEntity<String> criarPedido(@RequestBody Pedido pedido) {
         try{
@@ -27,29 +31,31 @@ public class PedidoController {
             return ResponseEntity.status(HttpStatus.CONFLICT).header("X-Error-Message", e.getMessage()).body("O valor do pedido têm que ser maior ou igual ao pedido mínimo do restaurante");
         }
     }
-
+    @Operation(summary = "Listar todos os pedidos")
     @GetMapping(value = "/listar")
     public ResponseEntity<Object> telaLista() {
         return ResponseEntity.ok(pedidoService.listarPedidos());
     }
-
+    @Operation(summary = "Listar pedidos por consumidor")
     @GetMapping("/listar-consumidor/{idConsumidor}")
     public ResponseEntity<List<Pedido>> getPedidosPorConsumidor(@PathVariable Long idConsumidor) {
         List<Pedido> pedidos = pedidoService.getPedidosPorConsumidor(idConsumidor);
         return ResponseEntity.ok(pedidos);
     }
+    @Operation(summary = "Listar pedidos por estabelecimento")
     @GetMapping("/listar-estabelecimento/{idEstabelecimento}")
     public ResponseEntity<List<Pedido>> getPedidosPorEstabelecimento(
             @PathVariable Long idEstabelecimento) {
         List<Pedido> pedidos = pedidoService.getPedidosPorEstabelecimento(idEstabelecimento);
         return ResponseEntity.ok(pedidos);
     }
-
+    @Operation(summary = "Excluir um pedido por ID")
     @DeleteMapping(value = "/{id}/excluir")
     public ResponseEntity<String> excluir(@PathVariable Long id) {
         pedidoService.excluirPedidoPorId(id);
         return ResponseEntity.ok("Pedido excluído com sucesso!");
     }
+    @Operation(summary = "Atualizar um pedido por ID")
     @PutMapping(value = "/{id}/alterar")
     public ResponseEntity<String> alterar(@PathVariable Long id, @Validated @RequestBody Pedido pedido){
         pedido = pedidoService.atualizarPedido(id,pedido);
